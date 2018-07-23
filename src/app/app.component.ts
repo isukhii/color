dd57fa31afb5d7c0a9fce011652f97ee17cbdaa2
Sukhveen Anand <sukhveen@Sukhveens-MacBook-Air.local>
2018-07-23 17:53:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  inputcolor:string;
+myStyle:{};
+color:string;
+
+  setstyle()
+{
+  this.myStyle= 
+  { 
+    
+    'color':this.generateRandomColor()
+    
+  }
+
+}
+generateRandomColor()
+{
+  this.color='#';
+  var letters=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  for(var i=0;i<6;i++)
+  {
+    this.color+=letters[Math.floor(Math.random()*16)];
+  }
+  console.log(this.color);
+  return this.color;
+}
+
 }
